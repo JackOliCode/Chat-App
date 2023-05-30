4a3f31540515e4ef948cdf2ee73c94ea71434bde
JackOliCode <jack.v.oliver@gmail.com>
2023-05-30 23:36:01 +0100

    Update CustomActions.js

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -1,10 +1,41 @@
 import { StyleSheet, View, Text, Button, TextInput, ImageBackground, Image, TouchableOpacity, Alert } from 'react-native';
 import { useEffect, useState } from 'react';
-
+import { useActionSheet } from '@expo/react-native-action-sheet';
 
 
 const CustomActions= ({ wrapperStyle, iconTextStyle }) => {
-    const onActionPress = () => {}
+    const actionSheet = useActionSheet();
+
+    /*onActionPress starts here
+        function displays an action menu that contains four options = an ActionSheet
+        when user selects one of these actions, a method for performing that action is called
+        */
+
+    const onActionPress = () => {
+        const options = ['Choose Image from Gallery', 'Take Picture', 'Send Location', 'Cancel']; //four options
+
+        const cancelButtonIndex = options.length -1;
+        actionSheet.showActionSheetWithOptions(
+            {
+              options,
+              cancelButtonIndex,
+            },
+            async (buttonIndex) => {
+              switch (buttonIndex) {
+                case 0:
+                  console.log('user wants to pick an image');
+                  return;
+                case 1:
+                  console.log('user wants to take a photo');
+                  return;
+                case 2:
+                  console.log('user wants to get their location');
+                default:
+              }
+            },
+          );
+        };
+    
 
 
     return (
@@ -32,9 +63,10 @@ const styles = StyleSheet.create({
       flex: 1,
     },
     iconText: {
+        padding: 10,
       color: '#b2b2b2',
       fontWeight: 'bold',
-      fontSize: 10,
+      fontSize: 20,
       backgroundColor: 'transparent',
       textAlign: 'center',
     },
